4fb4011228dbad0daef23bd2e25b39ff66ae7a9d
João Rosinhole <jvr131104@gmail.com>
2025-02-27 03:43:49 -0300

    polimento finalizado

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # DashboardTelefonica
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.0.7.
+## Como iniciar o projeto -------------------------------------------------------------------
+Para iniciar o projeto abra um terminal na pasta do projeto e rode o seguinte comando 'git checkout master' para ir para a branch principal
 
-Para iniciar o projeto rode em 1 terminal o comando 'npm run start'
-e em outro terminal 'json-server --watch src/db/db.json'
+depois rode no mesmo terminal o comando 'npm i --force' (o tailwind obriga o --force) para instalar os pacotes necessários do projeto
+
+após a instalação rode no mesmo terminal o comando 'npm run start' para iniciar o servidor web
+
+## ------------------------------------------------------------------------------------------
+
+e em outro terminal na pasta do projeto, rode o comando 'json-server --watch src/db/db.json' para iniciar o servidor JSON (Sem ele o programa não funciona)
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.0.7.
 
 ## Development server
 
--- src/app/components/form-planos/form-planos.component.ts
+++ src/app/components/form-planos/form-planos.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Plano } from '../../interfaces/interfaces';
 import { DbService } from '../../services/db.service';
-import { MatTableModule } from '@angular/material/table'
+import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
-import { PopupComponent } from "../reusable/popup/popup.component";
+import { PopupComponent } from '../reusable/popup/popup.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-planos',
@@ -11,23 +13,33 @@ import { PopupComponent } from "../reusable/popup/popup.component";
   imports: [
     MatTableModule,
     MatIconModule,
-    PopupComponent
-],
+    PopupComponent,
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './form-planos.component.html',
-  styleUrl: './form-planos.component.css'
+  styleUrl: './form-planos.component.css',
 })
 export class FormPlanosComponent {
   public tableData: Array<object> = [];
-  public displayedColumns: string[] = ['name', 'preco', 'franquiaDados', 'MinutosLigacao', 'acoes'];
+  public displayedColumns: string[] = [
+    'nome',
+    'preco',
+    'franquiaDados',
+    'MinutosLigacao',
+    'acoes',
+  ];
   public openPopup: boolean = false;
   public popupText: string = '';
+  public searchNome: string = '';
+  public searchPreco: number | null = null;
 
   private planData: Array<Plano> = [];
-  
-  constructor(private dbService: DbService) { }
+
+  constructor(private dbService: DbService) {}
 
   ngOnInit(): void {
-    this.getData();
+    this.getData(undefined);
     this.convertClientData();
   }
 
@@ -35,37 +47,70 @@ export class FormPlanosComponent {
     window.location.reload();
   }
 
-  public deleteData(id: number): void{
+  public deleteData(id: number): void {
     this.dbService.deletePlanData(id);
     window.location.reload();
   }
 
-  private getData(){
-    this.dbService.getPlanData(undefined)
-      .subscribe(data => {
-        (data as Array<Plano>).map(client => this.planData.push(client));
-      },
-    error =>{
-      this.popupText = `Ocorreu o seguinte erro ao tentar se conectar ao servidor ${error.message}`;
-      this.openPopup = true;
-    })
+  public search(type: string): void {
+    this.convertClientData();
+
+    if (type == 'nome') {
+      if(this.searchNome == ''){
+        this.convertClientData()
+      }
+      else{
+        this.tableData = this.tableData.filter((plan) => (plan as Plano).nome.toLocaleLowerCase() == this.searchNome.toLocaleLowerCase());
+      };
+    }
+    else {
+      if(!this.searchPreco){
+        this.convertClientData()
+      }
+      else{
+        this.tableData = this.tableData.filter((plan) => parseFloat((plan as Plano).preco) == this.searchPreco);
+      };
+    }
   }
 
-  private convertClientData(): void{
+  private getData(filter: string | undefined) {
+    if (filter) {
+      this.dbService.getPlanData(filter).subscribe(
+        (data) => {
+          (data as Array<Plano>).map((client) => this.planData.push(client));
+        },
+        (error) => {
+          this.popupText = `Ocorreu o seguinte erro ao tentar se conectar ao servidor ${error.message}`;
+          this.openPopup = true;
+        }
+      );
+    } else {
+      this.dbService.getPlanData(undefined).subscribe(
+        (data) => {
+          (data as Array<Plano>).map((client) => this.planData.push(client));
+        },
+        (error) => {
+          this.popupText = `Ocorreu o seguinte erro ao tentar se conectar ao servidor ${error.message}`;
+          this.openPopup = true;
+        }
+      );
+    }
+  }
+
+  private convertClientData(): void {
     let tableStructure: Array<object> = [];
 
-    this.planData.map(plan =>{
+    this.planData.map((plan) => {
       tableStructure.push({
         id: plan.id,
-        name: plan.nome,
+        nome: plan.nome,
         preco: plan.preco,
         franquiaDados: plan.franquiaDados,
         MinutosLigacao: plan.MinutosLigacao,
-        acoes: ''
-      })
-    })
+        acoes: '',
+      });
+    });
 
     this.tableData = tableStructure;
   }
-
 }
